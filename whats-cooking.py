f32e9f87f4662c251d21393f0b8a4f2d576ddb88
ddenis <rigel.cyrosea@gmail.com>
2015-12-13 21:49:44 +0200

    Fixed issues with saving sparse matrices (mmwrite doesn't allow bools)

--- whats-cooking.py
+++ whats-cooking.py
@@ -2,13 +2,15 @@ import numpy as np
 import pandas as pd
 from scipy import sparse
 from scipy import io as spio
-import sklearn as skl
+import sklearn.linear_model as sklm
 
 __author__ = 'Devin Denis'
 
 f = open("train.json")
 whats_cooking = pd.read_json(f)
 
+# To speed up testing
+# whats_cooking = whats_cooking.iloc[0:500, :]
 
 def get_all_ingredients():
     all_ingredients = set()
@@ -26,13 +28,11 @@ def create_ingredient_bool_columns():
 
 def create_csc_sparse_ing():
     create_ingredient_bool_columns()
-    relevant_cols = whats_cooking.iloc[:, 3:]
-    relevant_cols = relevant_cols.astype('bool')
-    coo_sparse_ing = sparse.coo_matrix(relevant_cols)
-    # for now
-    spio.mmwrite("coo_sparse_ing.mtx", coo_sparse_ing)
+    relevant_cols = whats_cooking.iloc[:, 3:].astype(int)
 
+    coo_sparse_ing = sparse.coo_matrix(relevant_cols)
     csc_sparse_ing = coo_sparse_ing.tocsc()
+
     spio.mmwrite("csc_sparse_ing.mtx", csc_sparse_ing)
     return csc_sparse_ing
 
@@ -42,7 +42,6 @@ def main():
         csc_sparse_ing = spio.mmread("csc_sparse_ing.mtx")
     except IOError:
         csc_sparse_ing = create_csc_sparse_ing()
-
 
 if __name__ == '__main__':
     main()
