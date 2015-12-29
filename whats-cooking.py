5f35fdd3c0b4e012a9e72f9e56d1ad729b076a71
ddenis <rigel.cyrosea@gmail.com>
2015-12-29 18:40:03 +0200

    Ingredient columns weren't being constructed correctly.  Testing a new method.

--- whats-cooking.py
+++ whats-cooking.py
@@ -11,7 +11,7 @@ f = open("train.json")
 whats_cooking = pd.read_json(f)
 
 # To speed up testing
-# whats_cooking = whats_cooking.iloc[0:500, :]
+whats_cooking = whats_cooking.iloc[0:500, :]
 
 def get_all_ingredients():
     all_ingredients = set()
@@ -22,9 +22,20 @@ def get_all_ingredients():
 
 
 def create_ingredient_bool_columns():
-    all_ingredients = get_all_ingredients()
-    for ingredient in all_ingredients:
-        whats_cooking[ingredient] = whats_cooking["ingredients"] == ingredient
+    # http://stackoverflow.com/questions/29034928/pandas-convert-a-column-of-list-to-dummies
+    print whats_cooking["ingredients"].head(10)
+    applied_series = whats_cooking["ingredients"].apply(pd.Series)  # Converts lists to dataframe
+    print applied_series.head(10)
+    stacked_series = applied_series.stack()  # Puts everything in one column again, creating a multi-level index
+    print stacked_series.head(10)
+    basic_dummies = pd.get_dummies(stacked_series)  # Gets indicator variables from the categories
+    print basic_dummies.head(10)
+    summed_dummies = basic_dummies.sum(level=0)  # Not sure if I need this.  Re-merges rows.
+    print summed_dummies.head(10)
+    # all_ingredients = get_all_ingredients()
+    # for ingredient in all_ingredients:
+    #     ing_column = whats_cooking["ingredients"] == ingredient
+    #     whats_cooking[ingredient] = ing_column
 
 
 def create_csc_sparse_ing():
@@ -58,11 +69,21 @@ def main():
 
     map_cuisines_to_nums(cuisine_mapping)
 
-    alg = sklm.LogisticRegression(multi_class='ovr')
+    y_true = whats_cooking["cuisine"].astype(int)
+    x_dense = csr_sparse_ing.todense()
 
-    scores = skcv.cross_val_score(alg, csr_sparse_ing, whats_cooking["cuisine"].astype(int), cv=3)
+    print x_dense.max()
+    print x_dense.min()
 
-    print scores.mean()
+    # alg = sklm.Lasso()
+    alg = sklm.LogisticRegression(penalty='l1', C=0.1, fit_intercept=False, multi_class='ovr')
+
+    alg.fit(csr_sparse_ing, y_true)
+
+    print "what now"
+    # scores = skcv.cross_val_score(alg, csr_sparse_ing, y_true, cv=3)
+
+    # print scores.mean()
 
 if __name__ == '__main__':
     main()
