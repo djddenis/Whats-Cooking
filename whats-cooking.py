e3f8ab012102376b93c93eae694928c5f0663db3
ddenis <rigel.cyrosea@gmail.com>
2016-01-02 18:51:49 +0200

    Successful full run: 70% correct

--- whats-cooking.py
+++ whats-cooking.py
@@ -11,7 +11,7 @@ f = open("train.json")
 whats_cooking = pd.read_json(f)
 
 # To speed up testing
-whats_cooking = whats_cooking.iloc[0:500, :]
+# whats_cooking = whats_cooking.iloc[0:5000, :]
 
 def get_all_ingredients():
     all_ingredients = set()
@@ -21,28 +21,18 @@ def get_all_ingredients():
     return all_ingredients
 
 
-def create_ingredient_bool_columns():
+def get_ingredient_bool_columns():
     # http://stackoverflow.com/questions/29034928/pandas-convert-a-column-of-list-to-dummies
-    print whats_cooking["ingredients"].head(10)
     applied_series = whats_cooking["ingredients"].apply(pd.Series)  # Converts lists to dataframe
-    print applied_series.head(10)
     stacked_series = applied_series.stack()  # Puts everything in one column again, creating a multi-level index
-    print stacked_series.head(10)
     basic_dummies = pd.get_dummies(stacked_series)  # Gets indicator variables from the categories
-    print basic_dummies.head(10)
-    summed_dummies = basic_dummies.sum(level=0)  # Not sure if I need this.  Re-merges rows.
-    print summed_dummies.head(10)
-    # all_ingredients = get_all_ingredients()
-    # for ingredient in all_ingredients:
-    #     ing_column = whats_cooking["ingredients"] == ingredient
-    #     whats_cooking[ingredient] = ing_column
+    return basic_dummies.sum(level=0)  # Unfolds multi level index to single level (how?)
 
 
 def create_csc_sparse_ing():
-    create_ingredient_bool_columns()
-    relevant_cols = whats_cooking.iloc[:, 3:].astype(int)
+    ing_cols = get_ingredient_bool_columns()
 
-    coo_sparse_ing = sparse.coo_matrix(relevant_cols)
+    coo_sparse_ing = sparse.coo_matrix(ing_cols)
     csr_sparse_ing = coo_sparse_ing.tocsr()
 
     spio.mmwrite("csr_sparse_ing.mtx", csr_sparse_ing)
@@ -70,20 +60,17 @@ def main():
     map_cuisines_to_nums(cuisine_mapping)
 
     y_true = whats_cooking["cuisine"].astype(int)
-    x_dense = csr_sparse_ing.todense()
+    # x_dense = csr_sparse_ing.todense()
 
-    print x_dense.max()
-    print x_dense.min()
 
     # alg = sklm.Lasso()
     alg = sklm.LogisticRegression(penalty='l1', C=0.1, fit_intercept=False, multi_class='ovr')
 
     alg.fit(csr_sparse_ing, y_true)
 
-    print "what now"
-    # scores = skcv.cross_val_score(alg, csr_sparse_ing, y_true, cv=3)
+    scores = skcv.cross_val_score(alg, csr_sparse_ing, y_true, cv=10)
 
-    # print scores.mean()
+    print scores.mean()
 
 if __name__ == '__main__':
     main()
